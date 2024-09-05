b406d9a953005cd384fbacfbd1e39030692af086
Gabriel Coutinho <122622768+Gabriel-M-Coutinho@users.noreply.github.com>
2024-09-05 20:04:13 -0300

    changed parameters of the sum function

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ let [x, y] = [5, 5];
 
 x += 10;
 
-function somar () {
-    return x + y;
+function somar (number1,number2) {
+    return number1 + number2;
 }
 
 console.log(somar());
 
 /*
 Nome: Gabriel Coutinho
-Semestre: Primeiro Semestre
-Curso: Desenvolvimento de Software Multiplataforma
+Semestre: Primeiro Semestre Desenvolvimento de Software Multiplataforma
 */ 
